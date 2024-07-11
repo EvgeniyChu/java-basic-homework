e91fb9b9bf5b1550350414ca120da121c09ad83c
Evgeniy Churakov <eugeniychurakov@gmail.com>
2024-07-11 22:12:16 +0400

    init

--- src/main/java/ru/evgeniychurakov/java/basic/homeworks/MainApplication.java
+++ src/main/java/ru/evgeniychurakov/java/basic/homeworks/MainApplication.java
@@ -1,7 +1,14 @@
 package ru.evgeniychurakov.java.basic.homeworks;
 
+
+import static ru.evgeniychurakov.java.basic.homeworks.lesson1.Application.*;
+
 public class MainApplication {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        greetings();
+        checkSign(1,2,-3);
+        selectColor();
+        compareNumbers();
+        addOrSubtractAndPrint(1,2,true);
     }
 }
