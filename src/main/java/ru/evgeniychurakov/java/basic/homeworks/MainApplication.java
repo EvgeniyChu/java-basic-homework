241dac6a8d518fcc0ecc58ab7d98059e2fad421f
Evgeniy Churakov <eugeniychurakov@gmail.com>
2024-07-31 21:18:59 +0400

    init

--- src/main/java/ru/evgeniychurakov/java/basic/homeworks/MainApplication.java
+++ src/main/java/ru/evgeniychurakov/java/basic/homeworks/MainApplication.java
@@ -1,26 +1,38 @@
 package ru.evgeniychurakov.java.basic.homeworks;
 
 
-import java.util.Arrays;
-
-import static ru.evgeniychurakov.java.basic.homeworks.lesson3.Application.*;
+import ru.evgeniychurakov.java.basic.homeworks.lesson4.Box;
+import ru.evgeniychurakov.java.basic.homeworks.lesson4.User;
 
 public class MainApplication {
     public static void main(String[] args) {
-        int[][] arr = {{1,2,3},{4,5,6}};
-        System.out.println(sumOfPositiveElements(arr));
 
-        printSquare(9);
+        User[] users = {new User("Петр", "Иванов", "Иванович", 1996, "ivanov@mail.ru"),
+                new User("Иван", "Петров", "Васильевич", 1980, "petrov@mail.ru"),
+                new User("Василий", "Сидоров", "Петрович", 1901, "sidorov@mail.ru"),
+                new User("Михаил", "Путин", "Ильич", 1977, "putin@mail.ru"),
+                new User("Петр", "Медведев", "Иванович", 2001, "medvedev@mail.ru"),
+                new User("Евгений", "Соколов", "Иванович", 1998, "sokolov@mail.ru"),
+                new User("Илья", "Норкин", "Васильевич", 1964, "norkin@mail.ru"),
+                new User("Роман", "Волков", "Петрович", 1962, "volkov@mail.ru"),
+                new User("Михаил", "Кошечкин", "Иванович", 1977, "cat@mail.ru"),
+                new User("Петр", "Собачкин", "Иванович", 1968, "dog@mail.ru")};
 
-        int[][] arr1 = {{1,2,3},{4,5,6},{7,8,9}};
-        nullifyDiagonals(arr1);
-        System.out.println(Arrays.deepToString(arr1));
+        for (int i = 0; i < users.length; i++) {
+            if(2024 - users[i].yearOfBirth > 40) {
+                users[i].info();
+            }
+        }
 
-        int[][] arr2 = {{1,2,3},{4,5,6},{7,100,9}};
-        System.out.println(findMax(arr2));
+        Box box = new Box(100,200, 300, "Green");
 
-        System.out.println(sumSecondRow(arr2));
-
+        box.info();
+        box.setColor("Red");
+        box.info();
+        box.open();
+        box.put();
+        box.get();
+        box.close();
 
     }
 }
