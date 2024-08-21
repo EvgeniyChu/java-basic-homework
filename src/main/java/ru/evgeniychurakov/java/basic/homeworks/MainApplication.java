4418b87c7d6637458a831ef53ca9948f96280ded
Evgeniy Churakov <eugeniychurakov@gmail.com>
2024-08-21 22:15:10 +0400

    Init

--- src/main/java/ru/evgeniychurakov/java/basic/homeworks/MainApplication.java
+++ src/main/java/ru/evgeniychurakov/java/basic/homeworks/MainApplication.java
@@ -1,42 +1,24 @@
 package ru.evgeniychurakov.java.basic.homeworks;
 
 
-import ru.evgeniychurakov.java.basic.homeworks.lesson9.Applcation;
-import ru.evgeniychurakov.java.basic.homeworks.lesson9.Employee;
+import ru.evgeniychurakov.java.basic.homeworks.lesson10.PhoneBook;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication {
     public static void main(String[] args) {
-        System.out.println(Applcation.list(1,5));
+        PhoneBook pb = new PhoneBook();
 
-        List<Integer> list = new ArrayList<>(Arrays.asList(1,4,5,6,8,10,14,5));
-        System.out.println(Applcation.sumElementsGreaterThan5(list));
+        pb.add("Igor", List.of(880055535555L));
+        pb.add("Eugene", List.of(88001234567L));
+        pb.add("Roman", List.of(89017005050L));
+        pb.add("Alex", List.of(88001110000L, 88008888888L));
 
-        List<Integer> list1 = new ArrayList<>(Arrays.asList(1,4,5,6,8,10,14,5));
-        Applcation.rewriteListByNumber(5,list1);
-        System.out.println(list1);
-
-        List<Integer> list2 = new ArrayList<>(Arrays.asList(1,4,5,6,8,10,14,5));
-        Applcation.addNumberToElements(5,list2);
-        System.out.println(list2);
-
-        List<Employee> employees = new ArrayList<>();
-        employees.add(new Employee("Michael", 20));
-        employees.add(new Employee("George",40));
-        employees.add(new Employee("Roman", 33));
-        employees.add(new Employee("Joe",78));
-        employees.add(new Employee("Donald",75));
-
-        System.out.println(Applcation.getNamesOfEmployees(employees));
-
-        System.out.println(Applcation.getEmployeesOlderThanAge(employees, 40));
-
-        System.out.println(Applcation.isAverageAgeGreaterThanAge(employees,50));
-
-        Applcation.youngestEmployee(employees);
+        System.out.println(pb);
+        pb.find("Alex");
+        pb.find("Alex1");
+        System.out.println(pb.containsPhone(89017005050L));
+        System.out.println(pb.containsPhone(88001110010L));
 
     }
 }
